e1763a2f355df205f11802e1ade0fc0ea6c7b608
mridulavijay <mridula.reghu@gmail.com>
2021-06-08 14:38:53 +0530

    server change

--- src/model/Authordata.js
+++ src/model/Authordata.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose');
-//mongoose.connect("mongodb+srv://userone:userone@ictakfiles.honye.mongodb.net/libraryapp?retryWrites=true&w=majority");
-mongoose.connect("mongodb://localhost:27017/librarycase")
+mongoose.connect("mongodb+srv://userone:userone@ictakfiles.honye.mongodb.net/libappcase?retryWrites=true&w=majority");
+//mongoose.connect("mongodb://localhost:27017/librarycase")
 const Schema=mongoose.Schema;
 const AuthorSchema= new Schema({
     author:String,
